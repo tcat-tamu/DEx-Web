e7d589595623784553f66b6a17f6b654af327e80
Matthew Barry <mbarry@tamu.edu>
2015-05-26 18:19:57 -0500

    initialize new web project

---
+++ releng/gulpfile.js
@@ -0,0 +1,145 @@
+var gulp = require('gulp');
+
+var amdOptimize = require('amd-optimize');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var merge = require('gulp-merge');
+var nunjucksCompile = require('gulp-nunjucks');
+var nunjucksRender = require('gulp-nunjucks-render');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var uglifyCSS = require('gulp-cssmin');
+var uglifyJS = require('gulp-uglify');
+
+
+var srcPath = '../src/';
+var stagingPath = '../build';
+var vendorPath = stagingPath + '/vendor';
+var distPath = '../dist/';
+var baseUrl = '/~matt.barry/dex-site';
+
+
+function makeCopyTask(source, dest) {
+   return function () {
+      return gulp.src(source).pipe(gulp.dest(dest));
+   };
+}
+
+
+
+gulp.task('stylesheets', ['fonts'], function () {
+   var vendorFiles = gulp.src([
+         vendorPath + '/bootstrap/dist/css/bootstrap.css',
+         vendorPath + '/font-awesome/css/font-awesome.css'
+      ])
+      .pipe(sourcemaps.init({
+         loadMaps: true
+      }));
+
+   var lessFiles = gulp.src([
+         srcPath + '/less/main.less'
+      ])
+      .pipe(sourcemaps.init())
+      .pipe(less())
+      .pipe(autoprefixer());
+
+   return merge(vendorFiles, lessFiles)
+      .pipe(concat('main.css'))
+      .pipe(uglifyCSS())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(distPath + '/css'));
+});
+
+
+gulp.task('templates', function () {
+   gulp.src(srcPath + '/js/templates/**/*.j2')
+      .pipe(nunjucksCompile({
+         name: function (file) {
+            // strip trailing '.j2' extension
+            return file.relative.slice(0, -3);
+         }
+      }))
+      .pipe(concat('templates.js'))
+      .pipe(gulp.dest(distPath + '/js'));
+});
+
+
+gulp.task('javascripts', function () {
+   var javascripts = gulp.src(srcPath + '/js/**/*.js')
+      .pipe(amdOptimize('main', {
+         findNestedDependencies: true,
+         paths: {
+            'bootstrap': vendorPath + '/bootstrap/dist/js/bootstrap',
+            'jquery': vendorPath + '/jquery/dist/jquery'
+         },
+         shim: {
+            'bootstrap': ['jquery']
+         },
+         exclude: ['config']
+      }))
+      .pipe(sourcemaps.init({
+         loadMaps: true
+      }))
+      .pipe(concat('main.js'));
+
+   var vendors = gulp.src([
+         vendorPath + '/almond/almond.js'
+      ])
+      .pipe(sourcemaps.init({
+         loadMaps: true
+      }))
+      .pipe(concat('vendors.js'));
+
+   var minified = merge(vendors, javascripts)
+      .pipe(uglifyJS())
+      .pipe(sourcemaps.write('.'));
+
+   var staticFiles = gulp.src([
+         srcPath + '/js/config.js'
+      ]);
+
+   return merge(minified, staticFiles)
+      .pipe(gulp.dest(distPath + '/js'));
+});
+
+
+gulp.task('html', function () {
+   nunjucksRender.nunjucks.configure(srcPath + '/html/', {
+      watch: false
+   });
+
+   return gulp.src([
+         srcPath + '/html/index.html.j2',
+         srcPath + '/html/browse.html.j2',
+         srcPath + '/html/browse-ms.html.j2'
+      ])
+      .pipe(nunjucksRender({
+         baseUrl: baseUrl
+      }))
+      .pipe(rename(function (path) {
+         path.basename = path.basename.replace(/\.html$/i, '');
+         path.ext = '.html';
+      }))
+      .pipe(gulp.dest(distPath));
+});
+
+
+gulp.task('images', makeCopyTask(srcPath + '/images/**/*', distPath + '/images'));
+gulp.task('fonts', makeCopyTask([
+      vendorPath + '/bootstrap/dist/fonts/*',
+      vendorPath + '/font-awesome/fonts/*'
+   ],
+   distPath + '/fonts'
+));
+
+gulp.task('default', ['html', 'images', 'javascripts', 'templates', 'stylesheets']);
+
+
+gulp.task('watch', ['default'], function () {
+   gulp.watch(srcPath + '/html/**/*.html.j2', ['html']);
+   gulp.watch(srcPath + '/images/**/*', ['images']);
+   gulp.watch(srcPath + '/js/**/*.js', ['javascripts']);
+   gulp.watch(srcPath + '/js/templates/**/*.j2', ['templates']);
+   gulp.watch(srcPath + '/less/**/*.less', ['stylesheets']);
+});
