c4f22f29fb2704aeae0dc0b22b9f25cc496b47ab
Matthew Barry <mbarry@tamu.edu>
2015-06-22 17:41:19 -0500

    putting layout, pagination, and search results together

--- src/js/testSearch.js
+++ src/js/testSearch.js
@@ -1,32 +1,54 @@
 define(function (require) {
 
    var $ = require('jquery');
+   var LayoutView = require('./search/views/layout_view');
    var ResultsView = require('./search/views/results_view');
-   var ResultTypes = require('./search/entities/extract_proxy');
+   var PaginatorView = require('./search/views/paginator_view');
+   // var ResultTypes = require('./search/entities/extract_proxy');
 
-   var ExtractProxyCollection = ResultTypes.ExtractProxyCollection;
+   var ExtractRepository = require('./search/extract_repository');
+
+
+   var repo = new ExtractRepository({
+      apiEndpoint: '/api/dex/extracts'
+   });
+
+   var layout = new LayoutView({
+      el: '#main'
+   });
+
+   layout.render();
+
+   function showResults(searchResponse) {
+      var resultsView = new ResultsView({
+         collection: searchResponse.results
+      });
+
+      var paginatorView = new PaginatorView({
+         current: searchResponse.currentPage,
+         total: searchResponse.numPages,
+         padding: 3
+      });
+
+      paginatorView.on('page', function (page) {
+         searchResponse.getPage(page).then(showResults);
+      });
+
+      layout.getRegion('results').show(resultsView);
+      layout.getRegion('pagination').show(paginatorView);
+
+   }
 
    $('#advancedSearch').on('submit', function (evt) {
       evt.preventDefault();
 
       var query = $('input#keyword').val();
 
-      $.ajax({
-         method: 'GET',
-         dataType: 'json',
-         url: '/api/dex/extracts/search',
-         data: {
-            q: query
-         },
-         success: function (searchResponse) {
-            var results = new ExtractProxyCollection(searchResponse.results);
-
-            var resultsView = new ResultsView({
-               collection: results
-            });
-
-            $('#main').html(resultsView.render().el);
-         }
-      });
+      repo.search(query, {
+         shelfmark: $('input#shelfmark').val(),
+         playwright: $('input#playwright').val(),
+         play: $('input#play').val(),
+         character: $('input#character').val()
+      }).then(showResults);
    });
 });
